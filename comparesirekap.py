da6eb3995032b4927e8e1e7f63d2ceaeaba10474
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-02-17 21:53:18 +0700

    Add new feature: region comparation

--- comparesirekap.py
+++ comparesirekap.py
@@ -3,10 +3,21 @@ import requests
 import pandas as pd
 import numpy as np
 import streamlit as st
+import webbrowser
 
-def compare(id):
+def compare(id,tingkat):
     #get data from api kawalpemilu.org
-    data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id="+str(id)).json()
+    
+    if tingkat == 'desa':
+        data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id="+str(id)).json()
+    elif tingkat == 'kec':
+        data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id="+str(id)[0:6]).json()
+    elif tingkat == 'kab':
+        data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id="+str(id)[0:4]).json()
+    elif tingkat == 'prov':
+        data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id="+str(id)[0:2]).json()
+    elif tingkat == 'nas':
+        data = requests.get("https://kp24-fd486.et.r.appspot.com/h?id=").json()
 
     aggregated_data = data["result"]["aggregated"]
 
@@ -17,14 +28,28 @@ def compare(id):
 
     df = pd.concat(dfs,ignore_index=True)
 
-    df = df[['idLokasi', 'pas1', 'pas2', 'pas3', 'dpt', 'name']].drop_duplicates(subset=['idLokasi'],keep='first')
+    if tingkat == 'desa':
+        df = df[['idLokasi', 'pas1', 'pas2', 'pas3', 'dpt', 'name']].drop_duplicates(subset=['idLokasi'],keep='first')
+    else: 
+        df = df[['idLokasi', 'pas1', 'pas2', 'pas3', 'totalTps', 'name']].drop_duplicates(subset=['idLokasi'],keep='first')
     df['status'] = np.where((df['pas1'] == 0) & (df['pas2'] == 0) & (df['pas3'] == 0), 0, 1)
-    df['idLokasi'] = id*1000 + df['name'].astype(int)
+    if tingkat == 'desa':
+        df['idLokasi'] = id*1000 + df['name'].astype(int)
     df['idLokasi'] = df['idLokasi'].astype(str)
     kawalpemilu = df
 
     #get data from api sirekap kpu
-    data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/"+str(id)[0:2]+"/"+str(id)[0:4]+"/"+str(id)[0:6]+"/"+str(id)+".json").json()
+    if tingkat == 'desa':
+        data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/"+str(id)[0:2]+"/"+str(id)[0:4]+"/"+str(id)[0:6]+"/"+str(id)+".json").json()
+    elif tingkat == 'kec':
+        data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/"+str(id)[0:2]+"/"+str(id)[0:4]+"/"+str(id)[0:6]+".json").json()
+    elif tingkat == 'kab':
+        data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/"+str(id)[0:2]+"/"+str(id)[0:4]+".json").json()
+    elif tingkat == 'prov':
+        data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/"+str(id)[0:2]+".json").json()
+    elif tingkat == 'nas':
+        data = requests.get("https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp.json").json()
+
     rows = []
 
     for id_, values in data['table'].items():
@@ -57,25 +82,42 @@ def compare(id):
     df['status'] = np.where((df['pas1'] == 0) & (df['pas2'] == 0) & (df['pas3'] == 0), 0, 1)
     kpu = df
 
-    #merge data sirekap kpu with kawalpemilu.org
-    compared = pd.merge(kawalpemilu, kpu, left_on='idLokasi', right_on='id', how='inner')
-    compared.columns = ['idLokasi', 'pas1_kawal', 'pas2_kawal', 'pas3_kawal', 'dpt', 'name', 'status_kawal',
-           'id', 'pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']
-    compared = compared[['id','dpt','pas1_kawal', 'pas2_kawal', 'pas3_kawal','status_kawal','pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']]
+    if tingkat == 'desa':
+        #merge data sirekap kpu with kawalpemilu.org
+        compared = pd.merge(kawalpemilu, kpu, left_on='idLokasi', right_on='id', how='inner')
+        compared.columns = ['idLokasi', 'pas1_kawal', 'pas2_kawal', 'pas3_kawal', 'dpt', 'name', 'status_kawal',
+               'id', 'pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']
+        compared = compared[['id','dpt','pas1_kawal', 'pas2_kawal', 'pas3_kawal','status_kawal','pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']]
 
-    #compare sirekap kpu with kawalpemilu.org
-    def check(row):
-        if row['status_kawal'] == 1 and row['status_kpu'] == 1:
-            if row['pas1_kawal'] == row['pas1_kpu'] and row['pas2_kawal'] == row['pas2_kpu'] and row['pas3_kawal'] == row['pas3_kpu']:
-                return 'sesuai' #detect ketidaksesuaian dengan kawalpemilu
+        #compare sirekap kpu with kawalpemilu.org
+        def check(row):
+            if row['status_kawal'] == 1 and row['status_kpu'] == 1:
+                if row['pas1_kawal'] == row['pas1_kpu'] and row['pas2_kawal'] == row['pas2_kpu'] and row['pas3_kawal'] == row['pas3_kpu']:
+                    return 'sesuai' #detect ketidaksesuaian dengan kawalpemilu
+                elif row['pas1_kpu'] + row['pas2_kpu'] + row['pas3_kpu'] > row['dpt']*1.02 or row['pas1_kawal'] + row['pas2_kawal'] + row['pas3_kawal'] > row['dpt']*1.02:
+                    return 'markup' #detect markup suara
+                else: return 'tidak sesuai' #detect ketidaksesuaian dengan kawalpemilu
             elif row['pas1_kpu'] + row['pas2_kpu'] + row['pas3_kpu'] > row['dpt']*1.02 or row['pas1_kawal'] + row['pas2_kawal'] + row['pas3_kawal'] > row['dpt']*1.02:
                 return 'markup' #detect markup suara
-            else: return 'tidak sesuai' #detect ketidaksesuaian dengan kawalpemilu
-        elif row['pas1_kpu'] + row['pas2_kpu'] + row['pas3_kpu'] > row['dpt']*1.02 or row['pas1_kawal'] + row['pas2_kawal'] + row['pas3_kawal'] > row['dpt']*1.02:
-            return 'markup' #detect markup suara
-        else: 
-            return 'belum dikawal' #detect tps belum dikawal/data belum ada di kawalpemilu
-    compared['check'] = compared.apply(check, axis=1)
+            else: 
+                return 'belum dikawal' #detect tps belum dikawal/data belum ada di kawalpemilu
+        compared['check'] = compared.apply(check, axis=1)
+    else:
+        #merge data sirekap kpu with kawalpemilu.org
+        compared = pd.merge(kawalpemilu, kpu, left_on='idLokasi', right_on='id', how='inner')
+        compared.columns = ['idLokasi', 'pas1_kawal', 'pas2_kawal', 'pas3_kawal', 'totalTps', 'name', 'status_kawal',
+               'id', 'pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']
+        compared = compared[['name','totalTps','pas1_kawal', 'pas2_kawal', 'pas3_kawal','status_kawal','pas1_kpu', 'pas2_kpu', 'pas3_kpu', 'status_kpu']]
+
+        #compare sirekap kpu with kawalpemilu.org
+        def check(row):
+            if row['status_kawal'] == 1 and row['status_kpu'] == 1:
+                if row['pas1_kawal'] == row['pas1_kpu'] and row['pas2_kawal'] == row['pas2_kpu'] and row['pas3_kawal'] == row['pas3_kpu']:
+                    return 'sesuai' #detect ketidaksesuaian dengan kawalpemilu
+                else: return 'tidak sesuai' #detect ketidaksesuaian dengan kawalpemilu
+            else: 
+                return 'belum dikawal' #detect tps belum dikawal/data belum ada di kawalpemilu
+        compared['check'] = compared.apply(check, axis=1)
     return compared
 
 # Fungsi untuk menambahkan warna baris berdasarkan kondisi
@@ -87,7 +129,7 @@ tps = pd.read_json("tps2.json",dtype=False)
 
 st.set_page_config(layout="wide")
 
-tab1, tab3 = st.tabs(["Suara TPS","Profil Paslon"])
+tab1, tab2, tab3 = st.tabs(["Suara TPS","Suara Wilayah","Profil Paslon"])
 
 with tab1:
     st.header("Sirekap KPU vs KawalPemilu.org")
@@ -115,10 +157,9 @@ with tab1:
     nm_desa = c4.selectbox('Pilih Desa/Kelurahan:', opsi_desa)
     id_desa = tps[(tps.index.astype(str).str.len() == 10) & (tps.index.astype(str).str.startswith(str(id_kec))) & (tps['id2name'] == nm_desa)].index[0]
 
-    #Input manual ID Desa/Kelurahan
+    # Menggunakan st.text_input() untuk memasukkan teks
     id = st.text_input("**Masukkan 10 digit ID Desa/Kel:** ",id_desa)
 
-    #Button menuju web Sirekap dan KawalPemilu
     c5, c6, c7 = st.columns([1,1,6])
     c5.link_button("🗳️ SIREKAP KPU", "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/"+str(id)[0:2]+"/"+str(id)[0:4]+"/"+str(id)[0:6]+"/"+str(id),use_container_width = True)
     c6.link_button("🔢 KAWAL PEMILU", "https://kawalpemilu.org/h/"+str(id),use_container_width = True)
@@ -127,11 +168,92 @@ with tab1:
                  <b>  KECAMATAN:</b> {tps.loc[int(str(id)[0:6]),'id2name']} | 
                  <b>  DESA/KEL:</b> {tps.loc[int(id),'id2name']}
                  """, unsafe_allow_html=True)
+
     id = int(id)
     # Menampilkan dataframe
-    df = compare(id)
+    df = compare(id, tingkat = 'desa')
     df = df.style.apply(row_color, axis=1)
-    st.dataframe(df, use_container_width = True)
+    st.dataframe(df,use_container_width = True)
+
+with tab2:
+    st.header("Sirekap KPU vs KawalPemilu.org")
+    st.markdown("Dokumentasi: [**github.com/rezkyyayang/kawalpemilu**](https://github.com/rezkyyayang/kawalpemilu)")
+
+    d1, d2, d3 = st.columns(3)
+
+    #Dropdown options PROVINSI
+    opsi_prov = tps[tps.index.astype(str).str.len() == 2]['id2name']
+    nm_prov = d1.selectbox('Pilih Provinsi:', opsi_prov, key='d_prov')
+    id_prov = tps[(tps.index.astype(str).str.len() == 2) & (tps['id2name'] == nm_prov)].index[0]
+
+    #Dropdown options KABUPATEN/KOTA
+    opsi_kab = tps[(tps.index.astype(str).str.len() == 4) & (tps.index.astype(str).str.startswith(str(id_prov)))]['id2name']
+    nm_kab = d2.selectbox('Pilih Kabupaten/Kota:', opsi_kab, key='d_kab')
+    id_kab = tps[(tps.index.astype(str).str.len() == 4) & (tps.index.astype(str).str.startswith(str(id_prov))) & (tps['id2name'] == nm_kab)].index[0]
+
+    #Dropdown options KECAMATAN
+    opsi_kec = tps[(tps.index.astype(str).str.len() == 6) & (tps.index.astype(str).str.startswith(str(id_kab)))]['id2name']
+    nm_kec = d3.selectbox('Pilih Kecamatan:', opsi_kec, key='d_kec')
+    id_kec = tps[(tps.index.astype(str).str.len() == 6) & (tps.index.astype(str).str.startswith(str(id_kab))) & (tps['id2name'] == nm_kec)].index[0]
+
+    id_wil = id_kec
+
+    tab_nasional, tab_provinsi, tab_kabkot, tab_kecamatan = st.tabs(['Nasional','Provinsi','Kab/Kota','Kecamatan'])
+
+    with tab_nasional:
+        st.subheader('Suara Nasional')
+        w1, w2, w3 = st.columns([1,1,6])
+        w1.link_button("🗳️ SIREKAP KPU", "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/",use_container_width = True)
+        w2.link_button("🔢 KAWAL PEMILU", "https://kawalpemilu.org/h/",use_container_width = True)
+        w3.write(f"""<b>  NASIONAL</b>
+                 """, unsafe_allow_html=True)
+        id = int(id_wil)
+        # Menampilkan dataframe
+        df = compare(id_wil, tingkat = 'nas')
+        df = df.style.apply(row_color, axis=1)
+        st.dataframe(df,use_container_width = True)
+
+    with tab_provinsi:
+        st.subheader('Suara Provinsi:')
+        x1, x2, x3 = st.columns([1,1,6])
+        x1.link_button("🗳️ SIREKAP KPU", "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/"+str(id_wil)[0:2],use_container_width = True)
+        x2.link_button("🔢 KAWAL PEMILU", "https://kawalpemilu.org/h/"+str(id_wil),use_container_width = True)
+        x3.write(f"""<b>  PROVINSI:</b> {tps.loc[int(str(id_wil)[0:2]),'id2name']}
+                 """, unsafe_allow_html=True)
+        id = int(id_wil)
+        # Menampilkan dataframe
+        df = compare(id_wil, tingkat = 'prov')
+        df = df.style.apply(row_color, axis=1)
+        st.dataframe(df,use_container_width = True)
+
+    with tab_kabkot:
+        st.subheader('Suara Kab/Kota:')
+        y1, y2, y3 = st.columns([1,1,6])
+        y1.link_button("🗳️ SIREKAP KPU", "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/"+str(id_wil)[0:2]+"/"+str(id_wil)[0:4],use_container_width = True)
+        y2.link_button("🔢 KAWAL PEMILU", "https://kawalpemilu.org/h/"+str(id_wil),use_container_width = True)
+        y3.write(f"""<b>  PROVINSI:</b> {tps.loc[int(str(id_wil)[0:2]),'id2name']} | 
+                 <b>  KAB/KOTA:</b> {tps.loc[int(str(id_wil)[0:4]),'id2name']}
+                 """, unsafe_allow_html=True)
+        id = int(id_wil)
+        # Menampilkan dataframe
+        df = compare(id_wil, tingkat = 'kab')
+        df = df.style.apply(row_color, axis=1)
+        st.dataframe(df,use_container_width = True)
+
+    with tab_kecamatan:
+        st.subheader('Suara Kecamatan:')
+        z1, z2, z3 = st.columns([1,1,6])
+        z1.link_button("🗳️ SIREKAP KPU", "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/"+str(id_wil)[0:2]+"/"+str(id_wil)[0:4]+"/"+str(id_wil),use_container_width = True)
+        z2.link_button("🔢 KAWAL PEMILU", "https://kawalpemilu.org/h/"+str(id),use_container_width = True)
+        z3.write(f"""<b>  PROVINSI:</b> {tps.loc[int(str(id_wil)[0:2]),'id2name']} | 
+                 <b>  KAB/KOTA:</b> {tps.loc[int(str(id_wil)[0:4]),'id2name']} |
+                 <b>  KECAMATAN:</b> {tps.loc[int(str(id_wil)),'id2name']}
+                 """, unsafe_allow_html=True)
+        id = int(id_wil)
+        # Menampilkan dataframe
+        df = compare(id_wil, tingkat = 'kec')
+        df = df.style.apply(row_color, axis=1)
+        st.dataframe(df,use_container_width = True)
 
 
 with tab3:
@@ -195,4 +317,5 @@ with tab3:
                      <br> 8. Indonesia bangsa terhormat di kancah internasional, serta pertahanan yang tangguh dan modern
                      """, unsafe_allow_html= True)
         st.link_button("📄 Unduh Visi Misi Lengkap", "https://drive.google.com/file/d/1ey8BwGJhDNcZXXSCkHgn621KdWRRbNWN/view?usp=sharing",use_container_width = True)
-    
+        
+
